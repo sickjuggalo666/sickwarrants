b7b137d23018fdba968c2771705c85cbd0696067
sickjuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2021-12-17 20:03:00 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,10 +3,17 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback('sickwarrants:getActive', function(source,cb,active)
     MySQL.Async.fetchAll('SELECT * FROM warrants WHERE active = @active',
-    {['@active'] = 1}, function(results)
+    {
+        ['@active'] = 1
+    }, function(results)
         local active = {}
             for i=1, #results do
-                table.insert(active,{name= results[i].firstname.." "..results[i].lastname,case= results[i].case,reason= results[i].reason,bday= results[i].bday})
+                table.insert(active,{
+                    name    = results[i].firstname.." "..results[i].lastname,
+                    case    = results[i].case,
+                    reason  = results[i].reason,
+                    bday    = results[i].bday
+                })
             end
         cb(active)
     end)
@@ -15,13 +22,21 @@ end)
 RegisterServerEvent('sickwarrants:createWarrant')
 AddEventHandler('sickwarrants:createWarrant', function(firstname,lastname,case,bday,reason)
     MySQL.Async.execute('INSERT INTO warrants (firstname, lastname, `case`, bday, reason, active) VALUES (@firstname, @lastname, @case, @bday, @reason, @active)',
-    {['@firstname'] = firstname,['@lastname'] = lastname,['@case'] = case,['@bday'] = bday,['@reason'] = reason,['@active'] = 1})
+    {
+        ['@firstname']  = firstname,
+        ['@lastname']   = lastname,
+        ['@case']       = case,
+        ['@bday']       = bday,
+        ['@reason']     = reason,
+        ['@active']     = 1})
 end)
 
 RegisterServerEvent('sickwarrants:DeleteWarrant')  -- Used to Delete when a Case # is entered!
 AddEventHandler('sickwarrants:DeleteWarrant', function(case)
     MySQL.Async.execute('DELETE FROM warrants WHERE `case` = @case',
-    {['@case'] = case},function()
+    {
+        ['@case'] = case
+    },function()
     end)
 end)
 
