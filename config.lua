533aed8d52f09bdb7d0c7dde29cda6760bc628a2
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2022-11-23 16:10:57 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,8 +2,6 @@ Config = {}
 
 Config.CheckVersion = true -- do you wanna stay up to date? will print in server console
 
-Config.UseMDT = true  -- Currently only set up for Deltarix MDT!
-
 Config.NotificationType = { --['okokNotify' / 'mythic' / 'esx' / 'chat' / 'custom' ]
     client = 'esx', 
 }
