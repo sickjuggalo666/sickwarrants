7f60992c1d838afba2179a006ccf7f4eda701128
SickJuggalo666 <82001106+sickjuggalo666@users.noreply.github.com>
2022-12-02 19:48:14 -0700

    Moved job check

--- config.lua
+++ config.lua
@@ -2,6 +2,16 @@ Config = {}
 
 Config.CheckVersion = true -- do you wanna stay up to date? will print in server console
 
+Config.jobsAuth = {
+	['police'] = true,
+	['bcso'] = true,
+}
+
+Config.BountyJobs = {
+      ['bondsman'] = true,
+      ['police'] = true
+}
+
 Config.NotificationType = { --['okokNotify' / 'mythic' / 'esx' / 'chat' / 'custom' ]
     client = 'okokNotify', 
     server = 'okokNotify'
